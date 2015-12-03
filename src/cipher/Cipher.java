ad76ad0b4208b4a6694c73362b43e5194f811bdb
kimena <kimenoah@gmail.com>
2015-12-02 22:00:13 -0800

    Added comments to Cipher

--- src/cipher/Cipher.java
+++ src/cipher/Cipher.java
@@ -1,12 +1,26 @@
 package cipher;
 
 
+/**
+ * A class used for encrypting and decrypting messages
+ * @author Noah Kime
+ */
 public abstract class Cipher {
 
 	protected Alphabet alph;
 	
+	/**
+	 * Encrypts a given message String using this Cipher strategy and returns the encoded result 
+	 * @param msg The given message String to be encrypted
+	 * @return The encoded version of the given message String
+	 */
 	public abstract String encrypt(String msg);
 	
+	/**
+	 * Decrypts a given message String using this Cipher strategy and returns the decoded result
+	 * @param msg The given message String to be decoded
+	 * @return The decoded version of the given message String
+	 */
 	public abstract String decrypt(String msg);
 	
 } //END Cipher
